38228453f378baad3cdef33ab5832e5f197fbfa1
spicypusheen <padme.garrapata@gmail.com>
2026-01-16 20:30:53 -0600

    Logo added

--- src/main.py
+++ src/main.py
@@ -6,13 +6,18 @@
 from tkinter import *
 
 window = Tk()
-window.title('Password Generator')
-window.config(padx=100, pady=50)
+window.title("Password Manager")
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width= 200, height= 224)
-lock_img = PhotoImage(file='../assets/tomato.png')
-canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text='00:00', font=(FONT_NAME, 35, 'bold'), fill='white')
-canvas.grid(column=1, row=1)
+# LOGO
+canvas = Canvas(window, width=200, height=200, highlightthickness=0)
+logo_img = PhotoImage(file="../assets/logo.png")
+logo_img = logo_img.subsample(3, 3)
+canvas.create_image(100, 90, image=logo_img)  # center-ish
+canvas.grid(row=0, column=1, pady=(0, 10))
+
 
 window.mainloop()
+
+
+
